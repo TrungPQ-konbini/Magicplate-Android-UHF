125c7983a445a7b079b2755947478e56a267f25f
TrungPQ <trungpq.konbini@gmail.com>
2022-05-24 17:51:18 +0700

    separate interval between Reading and detect an item removed

--- MagicPlateUHF/app/src/main/java/com/konbini/magicplateuhf/MainApplication.kt
+++ MagicPlateUHF/app/src/main/java/com/konbini/magicplateuhf/MainApplication.kt
@@ -88,7 +88,7 @@ class MainApplication : Application() {
                             Log.e("EKRON", "timeTagSizeChanged == 0L")
                         } else {
                             val offset = current - timeTagSizeChanged
-                            if (offset < AppSettings.Hardware.Comport.DelayTime.toLong()) {
+                            if (offset < AppSettings.Hardware.Comport.DelayTimeDetectTagsChange.toLong()) {
                                 Log.e(TAG, "$current | $offset => Ignore")
                                 Log.e("EKRON", "$current | $offset => Ignore")
                             } else {
@@ -105,7 +105,7 @@ class MainApplication : Application() {
                     Log.e("EKRON", "Clear AppContainer.GlobalVariable.listEPC")
                     AppContainer.GlobalVariable.listEPC.clear()
 
-                    Thread.sleep(AppSettings.Hardware.Comport.DelayTime.toLong())
+                    Thread.sleep(AppSettings.Hardware.Comport.DelayTimeReadTags.toLong())
 
                     // Start reading UHF
                     mReaderUHF.realTimeInventory(0xff.toByte(), 0x01.toByte())
@@ -237,7 +237,7 @@ class MainApplication : Application() {
                         mReaderUHF = RFIDReaderHelper.getDefaultHelper()
                         mReaderUHF.unRegisterObserver(rxObserver)
                         mReaderUHF.registerObserver(rxObserver)
-                        Thread.sleep(AppSettings.Hardware.Comport.DelayTime.toLong())
+                        Thread.sleep(AppSettings.Hardware.Comport.DelayTimeReadTags.toLong())
                         startRealTimeInventory()
                     } catch (ex: Exception) {
                         Log.e(SalesActivity.TAG, ex.toString())
